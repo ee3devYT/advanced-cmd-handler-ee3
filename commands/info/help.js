d40659f43d2d52492bcc9ea5953e5680d58c9c69
Vedant Mhapsekar <72020074+ee3devYT@users.noreply.github.com>
2021-03-23 17:11:08 +0530

    Update help.js

--- commands/info/help.js
+++ commands/info/help.js
@@ -15,11 +15,10 @@ module.exports = {
 	.setDescription('SUBSCRIBE TO EE3')
 	.setThumbnail('https://cdn.discordapp.com/avatars/817007659973869569/776c6070cdf6ab73a4127c7a16e2bafc.webp?size=4096')
 	.addFields(
-		{ name: 'Info Commands', value: '`avatar, ping`',},
-		{ name: 'Moderation Commands', value: '`kick, ban, unban`' },
-    { name: 'Server Commands (Anti Raid)', value: '`lock, unlock, lockserver true , lockserverfalse, slowmode`' },
-    { name: 'Fun', value: '`meme, image(+image banana)`' },
-    { name: 'Discord.js', value: '`djs docs`' },
+		{ name: 'Info Commands', value: '`help, ping`',},
+		{ name: 'name here', value: `commands here`' },
+    { name: 'name here', value: '`commands here`' },
+    { name: 'name here', value: '`commands here`' },
     { name: 'Links', value: '[YOUTUBE](https://youtube.com/channel/UCmJE0LBj20_7302l8596N7w) | [DISCORD](https://discord.gg/DpqZGduR)' },
     )
 
